98a21137f9b941dadd1e683c3f54d513bb749477
Eriq Augustine <eaugusti@chromium.org>
2013-06-09 02:20:59 -0700

    First version of the animation machine.

---
+++ client/test/animation.php
@@ -0,0 +1,15 @@
+<html>
+   <head>
+      <title>SPF Animation Test</title>
+      <link rel="stylesheet" type="text/css" media="all" href="css/animation.css">
+
+      <script type="text/javascript" src="js/jquery-2.0.2.min.js"></script>
+      <script type="text/javascript" src="js/animation.js"></script>
+   </head>
+   <body>
+      <div class='animation-test'>
+         <h1>Gem Animation</h1>
+         <div id='theGem' class='gem'></div>
+      </div>
+   </body>
+</html>
