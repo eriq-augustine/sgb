bdef8271a650561a5c2ae7e304d431c2ebef4266
Eriq Augustine <eriq.augustine@gmail.com>
2013-06-30 22:47:05 -0700

    Working on animations, will probably need a new animation engine.

---
+++ tools/spriteTest/index.php
@@ -0,0 +1,15 @@
+<html>
+   <head>
+      <title>SPF Animation Test</title>
+      <link rel="stylesheet" type="text/css" media="all" href="css/animations.css">
+
+      <script type="text/javascript" src="js/jquery-2.0.2.min.js"></script>
+      <script type="text/javascript" src="js/animation.js"></script>
+      <script type="text/javascript" src="js/animationTest.js"></script>
+   </head>
+   <body>
+      <div class='animation-area'>
+         <div id='animation-target' class='animation-gem'></div>
+      </div>
+   </body>
+</html>
