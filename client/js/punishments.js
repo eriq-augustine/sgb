073b339899d084ef602c3101c5f63d8ad8600282
Eriq Augustine <eaugusti@chromium.org>
2015-03-20 20:30:25 -0700

    Added all standard patterns.

--- client/js/punishments.js
+++ client/js/punishments.js
@@ -1,7 +1,7 @@
 "use static";
 
 punishments = {};
-punishments.NUM_PUNISHMENT_PATTERNS = 3;
+punishments.NUM_PUNISHMENT_PATTERNS = 8;
 punishments.patterns = [];
 
 function getPunishmentPattern(num) {
@@ -14,31 +14,69 @@ function getPunishmentPattern(num) {
 }
 
 function loadPatterns() {
-   // Ken
-   punishments.patterns.push(
-      [
-       [Gem.COLOR_RED, Gem.COLOR_RED, Gem.COLOR_RED, Gem.COLOR_RED, Gem.COLOR_RED, Gem.COLOR_RED],
-       [Gem.COLOR_GREEN, Gem.COLOR_GREEN, Gem.COLOR_GREEN, Gem.COLOR_GREEN, Gem.COLOR_GREEN, Gem.COLOR_GREEN],
-       [Gem.COLOR_BLUE, Gem.COLOR_BLUE, Gem.COLOR_BLUE, Gem.COLOR_BLUE, Gem.COLOR_BLUE, Gem.COLOR_BLUE],
-       [Gem.COLOR_YELLOW, Gem.COLOR_YELLOW, Gem.COLOR_YELLOW, Gem.COLOR_YELLOW, Gem.COLOR_YELLOW, Gem.COLOR_YELLOW]
-      ]);
-
    // Ryu
-   punishments.patterns.push(
-      [
-       [Gem.COLOR_RED, Gem.COLOR_GREEN, Gem.COLOR_BLUE, Gem.COLOR_YELLOW, Gem.COLOR_RED, Gem.COLOR_GREEN],
-       [Gem.COLOR_RED, Gem.COLOR_GREEN, Gem.COLOR_BLUE, Gem.COLOR_YELLOW, Gem.COLOR_RED, Gem.COLOR_GREEN],
-       [Gem.COLOR_RED, Gem.COLOR_GREEN, Gem.COLOR_BLUE, Gem.COLOR_YELLOW, Gem.COLOR_RED, Gem.COLOR_GREEN],
-       [Gem.COLOR_RED, Gem.COLOR_GREEN, Gem.COLOR_BLUE, Gem.COLOR_YELLOW, Gem.COLOR_RED, Gem.COLOR_GREEN]
-      ]);
+   punishments.patterns.push([
+      [Gem.COLOR_RED,   Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_YELLOW, Gem.COLOR_RED, Gem.COLOR_GREEN],
+      [Gem.COLOR_RED,   Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_YELLOW, Gem.COLOR_RED, Gem.COLOR_GREEN],
+      [Gem.COLOR_RED,   Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_YELLOW, Gem.COLOR_RED, Gem.COLOR_GREEN],
+      [Gem.COLOR_RED,   Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_YELLOW, Gem.COLOR_RED, Gem.COLOR_GREEN]
+   ]);
+
+   // Chun-Li
+   punishments.patterns.push([
+      [Gem.COLOR_RED,      Gem.COLOR_RED,    Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_BLUE],
+      [Gem.COLOR_RED,      Gem.COLOR_RED,    Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_BLUE],
+      [Gem.COLOR_YELLOW,   Gem.COLOR_YELLOW, Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_GREEN,  Gem.COLOR_GREEN],
+      [Gem.COLOR_YELLOW,   Gem.COLOR_YELLOW, Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_GREEN,  Gem.COLOR_GREEN]
+   ]);
+
+   // Sakura
+   punishments.patterns.push([
+      [Gem.COLOR_GREEN, Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_YELLOW],
+      [Gem.COLOR_GREEN, Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_YELLOW],
+      [Gem.COLOR_GREEN, Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_YELLOW],
+      [Gem.COLOR_GREEN, Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_YELLOW]
+   ]);
+
+   // Ken
+   punishments.patterns.push([
+      [Gem.COLOR_RED,      Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_RED],
+      [Gem.COLOR_GREEN,    Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_GREEN],
+      [Gem.COLOR_BLUE,     Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE],
+      [Gem.COLOR_YELLOW,   Gem.COLOR_YELLOW, Gem.COLOR_YELLOW, Gem.COLOR_YELLOW, Gem.COLOR_YELLOW, Gem.COLOR_YELLOW]
+   ]);
+
+   // Morrigan
+   punishments.patterns.push([
+      [Gem.COLOR_BLUE,     Gem.COLOR_YELLOW, Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_YELLOW, Gem.COLOR_BLUE],
+      [Gem.COLOR_BLUE,     Gem.COLOR_YELLOW, Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_YELLOW, Gem.COLOR_BLUE],
+      [Gem.COLOR_YELLOW,   Gem.COLOR_BLUE,   Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_YELLOW],
+      [Gem.COLOR_YELLOW,   Gem.COLOR_BLUE,   Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_YELLOW]
+   ]);
 
    // Hsien-Ko
-   punishments.patterns.push(
-      [[Gem.COLOR_YELLOW, Gem.COLOR_BLUE, Gem.COLOR_BLUE, Gem.COLOR_GREEN, Gem.COLOR_GREEN, Gem.COLOR_RED],
-       [Gem.COLOR_BLUE, Gem.COLOR_BLUE, Gem.COLOR_GREEN, Gem.COLOR_GREEN, Gem.COLOR_RED, Gem.COLOR_RED],
-       [Gem.COLOR_BLUE, Gem.COLOR_GREEN, Gem.COLOR_GREEN, Gem.COLOR_RED, Gem.COLOR_RED, Gem.COLOR_YELLOW],
-       [Gem.COLOR_GREEN, Gem.COLOR_GREEN, Gem.COLOR_RED, Gem.COLOR_RED, Gem.COLOR_YELLOW, Gem.COLOR_YELLOW]
-      ]);
+   punishments.patterns.push([
+      [Gem.COLOR_YELLOW,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_RED],
+      [Gem.COLOR_BLUE,     Gem.COLOR_BLUE,   Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_RED,    Gem.COLOR_RED],
+      [Gem.COLOR_BLUE,     Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_YELLOW],
+      [Gem.COLOR_GREEN,    Gem.COLOR_GREEN,  Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_YELLOW, Gem.COLOR_YELLOW]
+   ]);
+
+   // Donovan
+   punishments.patterns.push([
+      [Gem.COLOR_RED,   Gem.COLOR_YELLOW, Gem.COLOR_RED,    Gem.COLOR_YELLOW, Gem.COLOR_RED,    Gem.COLOR_YELLOW],
+      [Gem.COLOR_RED,   Gem.COLOR_YELLOW, Gem.COLOR_RED,    Gem.COLOR_YELLOW, Gem.COLOR_RED,    Gem.COLOR_YELLOW],
+      [Gem.COLOR_GREEN, Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE],
+      [Gem.COLOR_GREEN, Gem.COLOR_GREEN,  Gem.COLOR_GREEN,  Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_BLUE]
+   ]);
+
+   // Felicia
+   punishments.patterns.push([
+      [Gem.COLOR_GREEN, Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_YELLOW],
+      [Gem.COLOR_GREEN, Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_YELLOW],
+      [Gem.COLOR_GREEN, Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_YELLOW],
+      [Gem.COLOR_GREEN, Gem.COLOR_BLUE,   Gem.COLOR_BLUE,   Gem.COLOR_RED,    Gem.COLOR_RED,    Gem.COLOR_YELLOW]
+   ]);
 }
 
 // Instead of inducing a dependency on gems loading first,
